a08b90c669a5a53db94204385b05ff3d25736258
Vinnakota Saikrishna <vinnakotavnsaikrishna@gmail.com>
2021-06-20 00:53:07 -0500

    CRA Init and React components revision

---
+++ src/pages/App/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Speakers from '../Speakers/Speakers';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello!
+        </p>
+      </header>
+      <Speakers />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Speakers/Speakers.tsx
@@ -0,0 +1,59 @@
+import {data} from "../../speaker-data";
+
+const Speaker = () => {
+    const {id,bio,first,last,favorite,twitterHandle,company,sessions} = data[0];
+
+    return (
+        <div className="container speakers-list">
+            <div className="row">
+                {data.map(function (speaker) {
+                    const {
+                        id,
+                        bio,
+                        first,
+                        last,
+                        favorite,
+                        twitterHandle,
+                        company,
+                        sessions,
+                    } = speaker;
+
+                    return (
+                        <div className="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-sm-12 col-xs-12">
+                            <div className="card card-height p-4 mt-4">
+                                <div className="speaker-img d-flex flex-row justify-content-center align-items-center h-300">
+                                    <img
+                                        className="contain-fit"
+                                        src="https://via.placeholder.com/300"
+                                        width="300"
+                                        alt={`${first} ${last}`}
+                                    />
+                                </div>
+                                <div className="speaker-info">
+                                    <div className="d-flex justify-content-between mb-3">
+                                        <h3 className="text-truncate w-200">
+                                        {first} {last}
+                                        </h3>
+                                    </div>
+                                    <div>
+                                        <p>
+                                        {bio} {company} {twitterHandle} {favorite}
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="sessionBox card h-250">
+                                <span className="session w-100">
+                                {sessions[0].title}{" "}
+                                <strong>Room: {sessions[0].room.name}</strong>
+                                </span>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
+}
+
+export default Speaker;
