5c3156f08d3621ddc0fdce2fce79c0a46c45a4b8
Vinnakota Saikrishna <vinnakotavnsaikrishna@gmail.com>
2021-06-21 11:23:05 -0500

    Back to Basics!

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,20 +1,14 @@
-import Header from './components/Header';
-import Speakers from '../Speakers/Speakers';
 import './App.css';
-import Layout from './components/Layout';
-import { AuthProvider } from '../../common/contexts/AuthContext';
 
 function App() {
-
   return (
-    <AuthProvider initialLoggedInUser="Ronald">
-      <Layout startingTheme="light">
-        <div>
-          <Header />
-          <Speakers />
-        </div>
-      </Layout>
-    </AuthProvider>
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello!
+        </p>
+      </header>
+    </div>
   );
 }
 
