d1153548eea7f0ff283ceeae2fa7d57e84bfef6f
Vinnakota Saikrishna <vinnakotavnsaikrishna@gmail.com>
2021-06-21 22:24:41 -0500

    Cart using state

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -5,8 +5,43 @@ import { Routes, Route } from "react-router-dom";
 import Cart from "../Cart/Cart";
 import Detail from "../Detail/Detail";
 import Products from "../Products/Products";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [cart, setCart] = useState(() => {
+    try {
+      return JSON.parse(localStorage.getItem("cart") as any) ?? [];
+    } catch {
+      console.error("The cart could not be parsed into JSON.");
+      return [];
+    }
+  });
+
+  useEffect(() => localStorage.setItem("cart", JSON.stringify(cart)), [cart]);
+
+  function addToCart(id: any, sku: any) {
+    setCart((items: any) => {
+      const itemInCart = items.find((i: any) => i.sku === sku);
+      if (itemInCart) {
+        // Return new array with the matching item replaced
+        return items.map((i: any) =>
+          i.sku === sku ? { ...i, quantity: i.quantity + 1 } : i
+        );
+      } else {
+        // Return new array with the new item appended
+        return [...items, { id, sku, quantity: 1 }];
+      }
+    });
+  }
+
+  function updateQuantity(sku: any, quantity: any) {
+    setCart((items: any) => {
+      return quantity === 0
+        ? items.filter((i: any) => i.sku !== sku)
+        : items.map((i: any) => (i.sku === sku ? { ...i, quantity } : i));
+    });
+  }
+
   return (
     <>
       <div className="content">
@@ -15,8 +50,14 @@ export default function App() {
           <Routes>
             <Route path="/" element={<h1>Welcome to Carved Rock Fitness</h1>} />
             <Route path="/:category" element={<Products />} />
-            <Route path="/:category/:id" element={<Detail />} />
-            <Route path="/cart" element={<Cart />} />
+            <Route 
+              path="/:category/:id" 
+              element={<Detail addToCart={addToCart} />} 
+            />
+            <Route 
+              path="/cart" 
+              element={<Cart cart={cart} updateQuantity={updateQuantity} />} 
+            />
           </Routes>
         </main>
       </div>
--- src/pages/Detail/Detail.tsx
+++ src/pages/Detail/Detail.tsx
@@ -1,11 +1,13 @@
+import React, { useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import Spinner from "../../common/components/Spinner";
 import useFetch from "../../common/hooks/useFetch";
 import PageNotFound from "../App/PageNotFound";
 
-export default function Detail() {
+export default function Detail(props: any) {
   const { id } = useParams();
   const navigate = useNavigate();
+  const [sku, setSku] = useState("");
   const { data: product, loading, error } = useFetch(`products/${id}`);
 
   if (loading) return <Spinner />;
@@ -17,8 +19,25 @@ export default function Detail() {
       <h1>{(product as any).name}</h1>
       <p>{(product as any).description}</p>
       <p id="price">${(product as any).price}</p>
+
+      <select id="size" value={sku} onChange={(e) => setSku(e.target.value)}>
+        <option value="">What size?</option>
+        {(product as any).skus.map((s: any) => (
+          <option key={s.sku} value={s.sku}>
+            {s.size}
+          </option>
+        ))}
+      </select>
+
       <p>
-        <button className="btn btn-primary" onClick={() => navigate("/cart")}>
+        <button
+          disabled={!sku}
+          className="btn btn-primary"
+          onClick={() => {
+            props.addToCart(id, sku);
+            navigate("/cart");
+          }}
+        >
           Add to cart
         </button>
       </p>
