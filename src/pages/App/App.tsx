a253e5920ca6474785ec13a713f5e37d91ecd8b4
Vinnakota Saikrishna <vinnakotavnsaikrishna@gmail.com>
2021-06-24 08:48:53 -0500

    Inline CSS

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,13 +1,29 @@
 import './App.css';
+import Newsletter from './components/Newsletter';
+import { ThemeProvider } from './contexts/ThemeContext';
+
+// TODO: separate before/after
+// Get before in a place ready to screen record
+const theme = {
+  header: {
+    fg: '#ff598a',
+  },
+  input: {
+    color: '#fff',
+    background: '#070222',
+    textAlign: 'center',
+  },
+  inputFocus: {
+    outline: '2px solid #5e9fff',
+  },
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello!
-        </p>
-      </header>
+    <div className="app__newsletter">
+      <ThemeProvider theme={theme}>
+        <Newsletter />
+      </ThemeProvider>
     </div>
   );
 }
